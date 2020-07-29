1365cac8e19d303abd7ce4a850ba2580a4fdf481
test1230-lab <64430532+test1230-lab@users.noreply.github.com>
2020-07-29 14:25:42 -0400

    Update test.py

--- test.py
+++ test.py
@@ -15,21 +15,21 @@ import cv2
 
 def vis_parsing_maps(im, parsing_anno, stride, save_im=False, save_path='vis_results/parsing_map_on_im.jpg'):
     # Colors for all 20 parts
-    part_colors = [[255, 0, 0], [255, 85, 0], [255, 170, 0],
-                   [255, 0, 85], [255, 0, 170],
-                   [0, 255, 0], [85, 255, 0], [170, 255, 0],
-                   [0, 255, 85], [0, 255, 170],
-                   [0, 0, 255], [85, 0, 255], [170, 0, 255],
-                   [0, 85, 255], [0, 170, 255],
-                   [255, 255, 0], [255, 255, 85], [255, 255, 170],
-                   [255, 0, 255], [255, 85, 255], [255, 170, 255],
-                   [0, 255, 255], [85, 255, 255], [170, 255, 255]]
+    part_colors = [[0, 0, 0], [255, 255, 255], [255, 255, 255],
+                   [255, 255, 255], [255, 255, 255],
+                   [255, 255, 255], [0, 0, 0], [0, 0, 0],
+                   [0, 0, 0], [0, 0, 0],
+                   [255, 255, 255], [255, 255, 255], [255, 255, 255],
+                   [255, 255, 255], [0, 0, 0],
+                   [0, 0, 0], [0, 0, 0], [0, 0, 0],
+                   [0, 0, 0], [0, 0, 0], [0, 0, 0],
+                   [0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
     im = np.array(im)
     vis_im = im.copy().astype(np.uint8)
     vis_parsing_anno = parsing_anno.copy().astype(np.uint8)
     vis_parsing_anno = cv2.resize(vis_parsing_anno, None, fx=stride, fy=stride, interpolation=cv2.INTER_NEAREST)
-    vis_parsing_anno_color = np.zeros((vis_parsing_anno.shape[0], vis_parsing_anno.shape[1], 3)) + 255
+    vis_parsing_anno_color = np.zeros((vis_parsing_anno.shape[0], vis_parsing_anno.shape[1], 3)) + 0
 
     num_of_class = np.max(vis_parsing_anno)
 
@@ -39,7 +39,7 @@ def vis_parsing_maps(im, parsing_anno, stride, save_im=False, save_path='vis_res
 
     vis_parsing_anno_color = vis_parsing_anno_color.astype(np.uint8)
     # print(vis_parsing_anno_color.shape, vis_im.shape)
-    vis_im = cv2.addWeighted(cv2.cvtColor(vis_im, cv2.COLOR_RGB2BGR), 0.4, vis_parsing_anno_color, 0.6, 0)
+    vis_im = cv2.addWeighted(cv2.cvtColor(vis_im, cv2.COLOR_RGB2BGR), 0, vis_parsing_anno_color, 1, 0)
 
     # Save result or not
     if save_im:
@@ -85,6 +85,6 @@ def evaluate(respth='./res/test_res', dspth='./data', cp='model_final_diss.pth')
 
 
 if __name__ == "__main__":
-    evaluate(dspth='/home/zll/data/CelebAMask-HQ/test-img', cp='79999_iter.pth')
+    evaluate(dspth='/home/test/data/CelebAMask-HQ/test-img', cp='79999_iter.pth')
 
 
