d64aed3c9fa6a8acf1eb5889dfb3aa231fe98201
test1230-lab <64430532+test1230-lab@users.noreply.github.com>
2020-07-29 22:05:42 -0400

    Update head.py

--- head.py
+++ head.py
@@ -19,8 +19,8 @@ if value == '?':
 	print("outputs semi-transparent mask on the image. Example: https://imgur.com/a/bPbKbvF")
 	value = input("test mode? y/n/? ")
 if value == 'y':
-	a = (0.4)
-	b=(0.1)
+	a = (0.6)
+	b=(0.3)
 	c=(1)
 if value == 'n':
 	a = (0)
